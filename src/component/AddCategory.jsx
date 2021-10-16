8c2540387df0008de3b331f78a2b40ff36e35ec8
wthoutjc <juancamilorr2012@hotmail.com>
2021-10-15 20:30:47 -0500

    .

--- src/component/AddCategory.jsx
+++ src/component/AddCategory.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import PropTypes from 'prop-types';
 
 const AddCategory = ({setCategories}) => {
-  const [inputValue, setInputValue] = useState(null);
+  const [inputValue, setInputValue] = useState('');
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
